a5d4b0d9320152daccf2119d659f8aba01cc57ad
Ananda Ravi Kuntadi <anandaravik@gmail.com>
2024-05-22 11:30:46 +0700

    Add Database Migrate for Banquet

---
+++ database/migrations/2024_05_22_041923_create_kamars_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('kamars', function (Blueprint $table) {
+            $table->id('idKamar');
+            $table->integer('noKamar');
+            $table->string('tipe');
+            $table->integer('kapasitas');
+            $table->boolean('status');
+            $table->integer('harga');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('kamars');
+    }
+};
