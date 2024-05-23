7eb438a78400478f5c2acf1da6d9e3fac143eefc
Ananda Ravi Kuntadi <anandaravik@gmail.com>
2024-05-23 21:15:14 +0700

    Fixing Merge#4

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,12 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
-use App\Http\Middleware\AdminAuthMiddleware;
-use App\Http\Controllers\AdminAuthController;
+use App\Http\Controllers\dashboardController;
 
-Route::get('/', function () {
-    return view('dashboard.index');
-});
 
+Route::get('/', [dashboardController::class,'index']);
+Route::get('/packets');
+Route::get('/orders');
+Route::get('/income');
 Route::get('/history');
-    Route::get('/admin/dashboard', [AdminAuthController::class, 'dashboard'])->name('admin.dashboard');
-
-Route::get('/admin/login', [AdminAuthController::class, 'showLoginForm'])->name('admin.login');
-Route::post('/admin/login', [AdminAuthController::class, 'login']);
-Route::post('/admin/logout', [AdminAuthController::class, 'logout'])->name('admin.logout');
+Route::get('/profile');
+Route::get('/docs');
