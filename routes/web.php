e4c861499eedec853c312d575a7ca2c336e1222e
Ananda Ravi Kuntadi <anandaravik@gmail.com>
2024-05-25 01:46:04 +0700

    Adding Tambah Pesanan in index order

--- routes/web.php
+++ routes/web.php
@@ -28,12 +28,12 @@ Route::middleware('auth:admin')->group(function () {
     Route::get('/admin/ruangan/{id_ruangan}/edit', [RuanganController::class, 'edit'])->name('admin.ruangan.edit');
     Route::put('/admin/ruangan/{id_ruangan}', [RuanganController::class, 'update'])->name('admin.ruangan.update');
     Route::delete('/admin/ruangan/{id_ruangan}', [RuanganController::class, 'destroy'])->name('admin.ruangan.destroy');
-    Route::get('/admin/reservasi/create', [OrderController::class, 'create'])->name('admin.reservasi.create');
-    Route::post('/admin/reservasi', [OrderController::class, 'store'])->name('admin.reservasi.store');
+    Route::get('/admin/orders/create', [OrderController::class, 'create'])->name('admin.order.create');
+    Route::post('/admin/orders', [OrderController::class, 'store'])->name('admin.order.store');
     Route::put('/admin/orders/{id}/cancel', [OrderController::class, 'cancel'])->name('admin.order.cancel');
     Route::delete('/admin/orders/{id}/delete', [OrderController::class, 'delete'])->name('admin.order.delete');
     Route::get('/admin/orders/{id}/checkin', [OrderController::class, 'showCheckinForm'])->name('admin.order.checkin');
-    Route::post('/admin/orders/{id}/processCheckin', [OrderController::class, 'processCheckin'])->name('admin.order.processCheckin');// routes/web.php
+    Route::post('/admin/orders/{id}/processCheckin', [OrderController::class, 'processCheckin'])->name('admin.order.processCheckin');
 
     Route::put('/admin/orders/{id}/checkout', [OrderController::class, 'checkout'])->name('admin.order.checkout');
     Route::post('/admin/orders/{id}/upload', [OrderController::class, 'upload'])->name('admin.order.upload');
