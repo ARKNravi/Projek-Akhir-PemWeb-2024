34585125c22cdc3ba84242723f8ebb7a2c8b8c21
Ananda Ravi Kuntadi <anandaravik@gmail.com>
2024-06-03 14:35:55 +0700

    test

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -9,14 +9,14 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            AdminSeeder::class,
-            SessionSeeder::class,
-            PemesanSeeder::class,
-            PaymentSeeder::class,
-            PaketSeeder::class,
-            MakananSeeder::class,
-            LayoutSeeder::class,
-            RuanganSeeder::class,
+            // AdminSeeder::class,
+            // SessionSeeder::class,
+            // PemesanSeeder::class,
+            // PaymentSeeder::class,
+            // PaketSeeder::class,
+            // MakananSeeder::class,
+            // LayoutSeeder::class,
+            // RuanganSeeder::class,
             OrderSeeder::class,
         ]);
     }
